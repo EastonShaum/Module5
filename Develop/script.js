b8c6be8e0c82b042b82d636b9290c7bc19f23a58
Easton Shaum <Eastonshaum@gmail.com>
2021-06-07 13:24:15 -0600

    more logic for saving the new inputs

--- Develop/script.js
+++ Develop/script.js
@@ -3,20 +3,27 @@ console.log(today.format("dddd, MMMM Do"));
 
 $("#currentDay").append(today.format("dddd, MMMM Do YYYY"));
 
-var events = {};
+var events = [];
 
 
-
-// task text was clicked
-$(".hours-group").on("click", "p", function() {
+// event text was clicked
+$(".hours-group").on("click", function() {
     console.log("click");
     // get current text of p element
     var text = $(this)
       .text()
       .trim();
-  
+
+    var status = $(this)
+      .closest(".hours-group")
+      .attr("id")
+      .replace("hour-", "");
+    console.log(status);
     // replace p element with a new textarea
-    var textInput = $("<textarea>").addClass("form-control").val(text);
+    var textInput = $("<textarea>")
+      .attr('id', "hour-" + status)
+      .addClass("form-control")
+      .val(text);
     $(this).replaceWith(textInput);
   
     // auto focus new element
@@ -24,31 +31,34 @@ $(".hours-group").on("click", "p", function() {
   });
 
 // editable field was un-focused
-$(".hours-group").on("blur", "textarea", function() {
-  console.log("click off");
+$(document).on("blur", ".form-control", function() {
+  
   // get current value of textarea
   var text = $(this).val();
-
+  
   // get status type and position in the list
   var status = $(this)
-    .closest(".hours-group")
+    
     .attr("id")
-    .replace("list-", "");
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
+    .replace("hour-", "");
+    console.log(status);
 
-  // update task in array and re-save to localstorage
-  events[status][index].text = text;
+  // update task in array
+  events[status] = text;
+
+  localStorage.setItem("events", JSON.stringify(events));
 
   // recreate p element
   var eventP = $("<p>")
-    .addClass("m-1")
+    .attr('id', "hour-" + status)
+    .addClass("hours-group col-10")
     .text(text);
 
   // replace textarea with new content
   $(this).replaceWith(eventP);
 });
+
+
 
   // blocks in the past are gray
 
@@ -56,24 +66,19 @@ $(".hours-group").on("blur", "textarea", function() {
 
   // future time blocks are green
 
-// var checkEvent = function(eventEl) {
-//     // get check hour from event element
-//     var date = $(taskEl)
-//       .find("span")
-//       .text()
-//       .trim();
+// var checkEvent = function(hour) {
   
-//     // convert to moment object at 5:00pm
-//     var time = moment(date, "L").set("hour", 17);
+//     var currentHour = moment().format("H");
+    
   
 //     // remove any old classes from element
-//     $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+//     $(eventEl).removeClass("list-group-item-warning list-group-item-danger");
   
 //     // apply new class if task is near/over due date
 //     if (moment().isAfter(time)) {
-//       $(taskEl).addClass("list-group-item-danger");
+//       $(eventEl).addClass("list-group-item-danger");
 //     } else if (Math.abs(moment().diff(time, "days")) <= 2) {
-//       $(taskEl).addClass("list-group-item-warning");
+//       $(eventEl).addClass("list-group-item-warning");
 //     }
 //  };
 
@@ -83,30 +88,27 @@ $(".hours-group").on("blur", "textarea", function() {
     // if nothing in localStorage, create a new object to track all task status arrays
     if (!events) {
       events = {
-        0: ["Wake up"],
-        1: [],
+        0: ["Wake up, Eat breakfast"],
+        1: ["First appointment"],
         2: [],
-        3: [],
+        3: ["Lunch"],
         4: [],
-        5: [],
+        5: ["Second appointment"],
         6: [],
         7: [],
-        8: []
+        8: ["Go home"]
       };
     }
+    localStorage.setItem("events", JSON.stringify(events));
+
     for (i = 0; i < 9 ; i++) {
+
       console.log(events[i]);
-      $("#hour-" + i).append(events[i]);
+      $("#hour-" + i).append(events[i] + " ");
+
+      //checkEvent(i);
     };
-    // // loop over object properties
-    // $.each(events, function(list, arr) {
-    //   // then loop over sub-array
-    //   arr.forEach(function(event) {
-    //  console.log(events[0]);
-    //     $("#hour-" + eventList).append(event[arr]);
-    //     createEvent(event.text, list);
-    //   });
-    // });
+    //saveAllEvents();
   };
 
   var createEvent = function(eventText, eventList) {
@@ -115,12 +117,48 @@ $(".hours-group").on("blur", "textarea", function() {
     //   .text(eventText);
     console.log(eventText);
     // check due date
-    //auditTask(taskLi);
+    //checkEvent();
   
-    // append to ul list on the page
+    // append to the page
     $("#hour-" + eventList).append(eventP);
     console.log(eventP);
     console.log(eventList);
   };
+
+  var saveEvents = function(hour, text) {
+    var events = JSON.parse(localStorage.getItem(events));
+    events[hour] = text
+    localStorage.setItem("events", JSON.stringify(events));
+  };
+
+// repeat for every save button or is there a faster way?
+  // save button was clicked
+$(".saveBtn").click(function() {
+  console.log("save clicked");
+  
+  var index = 0
+    // $(this)
+    // .closest("p")
+    // .attr("id")
+    // .replace("hour-", "");
+    console.log(index);
+    
+  // get form values
+  var eventText = document.getElementById("hour-" + index).textContent;
+  console.log(eventText);
+
+  if (eventText ) {
+    createEvent(eventText, 0);
+
+    // save in events array
+    events[index].push({
+      text: taskText
+    });
+
+    saveEvents();
+  }
+});
+
+
 
 loadEvents();
